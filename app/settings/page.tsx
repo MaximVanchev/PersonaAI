f65e51cc475556cee172f8e3d84dac7f616a39b9
maximvanchev <maxim.van.mv@gmail.com>
2025-10-25 00:35:48 +0300

    feat: implement SettingsForm component and create Settings page; remove unused toast notification logic

---
+++ app/settings/page.tsx
@@ -0,0 +1,6 @@
+import SettingsForm from "@/components/forms/SettingsForm";
+
+
+export default function Settings() {
+    return SettingsForm();
+}
---
+++ components/forms/SettingsForm.tsx
@@ -0,0 +1,14 @@
+
+
+export default function SettingsForm() {
+  return (
+    <div className="min-h-screen flex flex-col">
+        <form className="ml-100 flex-row mr-100 mt-50 p-10 bg-[#474242] rounded-4xl">
+            <div className="text-[3rem] flex justify-center">
+                Settings
+            </div>
+            
+    </form>
+    </div>
+  );
+}
